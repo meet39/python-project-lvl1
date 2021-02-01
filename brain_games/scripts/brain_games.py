37f5047c25bf36b74fbb42e5126171b311be97dd
Ilia <ilia.ostrovskij@gmail.com>
2021-02-01 05:24:21 +0300

    add new feature Welcome

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+from brain_games.cli import welcome_user
 
-def greeting():
+
+def welcome():
     print('Welcome to the Brain Games!')
 
 def main():
-    greeting()
+    welcome()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
